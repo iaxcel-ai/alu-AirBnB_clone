14fa88de624c9492c5206e7a0e25f3a113a0485b
iaxcel-ai <i.axcel@alustudent.com>
2026-06-03 13:00:00 +0200

    feat: add Amenity, Place, and Review models

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines the Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Represents an Amenity."""
+
+    name = ""
