85fab1ec535eacdc3640be0e67b616f03ad17666
iaxcel-ai <i.axcel@alustudent.com>
2026-06-03 10:00:00 +0200

    feat: implement FileStorage engine with JSON serialization

---
+++ models/engine/file_storage.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""This module defines the FileStorage class."""
+import json
+
+
+class FileStorage:
+    """Serializes instances to a JSON file and deserializes back."""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Return the dictionary __objects."""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Set in __objects the obj with key <obj class name>.id."""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serialize __objects to the JSON file (path: __file_path)."""
+        serialized = {}
+        for key, obj in FileStorage.__objects.items():
+            serialized[key] = obj.to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            json.dump(serialized, f)
+
+    def reload(self):
+        """Deserialize the JSON file to __objects, if the file exists."""
+        try:
+            from models.base_model import BaseModel
+            from models.user import User
+            from models.state import State
+            from models.city import City
+            from models.amenity import Amenity
+            from models.place import Place
+            from models.review import Review
+
+            classes = {
+                "BaseModel": BaseModel,
+                "User": User,
+                "State": State,
+                "City": City,
+                "Amenity": Amenity,
+                "Place": Place,
+                "Review": Review,
+            }
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                data = json.load(f)
+            for key, value in data.items():
+                class_name = value.get("__class__")
+                if class_name in classes:
+                    FileStorage.__objects[key] = classes[class_name](**value)
+        except Exception:
+            pass
